f6b51b0057d5566aed37a146413c51d60647d239
Arialdo Martini <arialdomartini@gmail.com>
2014-01-13 10:57:05 +0100

    No more inconsistent spaces/tabs in Basket.cs

--- src/SalesTax/Basket.cs
+++ src/SalesTax/Basket.cs
@@ -6,49 +6,49 @@ using SalesTax.TaxCalculators;
 
 namespace SalesTax
 {
-    public class Basket
-    {
-	    private readonly ITaxCalculator _taxCalculator;
-	    private readonly List<ICanBeSold> _items;
-	    private IReceiptFormatter _receiptFormatter;
+	public class Basket
+	{
+		private readonly ITaxCalculator _taxCalculator;
+		private readonly List<ICanBeSold> _items;
+		private IReceiptFormatter _receiptFormatter;
 
-	    public Basket(ITaxCalculator taxCalculator, IReceiptFormatter receiptFormatter, List<ICanBeSold> items)
+		public Basket(ITaxCalculator taxCalculator, IReceiptFormatter receiptFormatter, List<ICanBeSold> items)
 		{
 			_taxCalculator = taxCalculator;
-		    _receiptFormatter = receiptFormatter;
-		    _items = items;
+			_receiptFormatter = receiptFormatter;
+			_items = items;
 		}
 
 		public Basket(ITaxCalculator taxCalculator, IReceiptFormatter receiptFormatter, ICanBeSold item) : this(taxCalculator, receiptFormatter, new List<ICanBeSold> { item }) { }
 
 		public Basket(ITaxCalculator taxCalculator, IReceiptFormatter receiptFormatter) : this(taxCalculator, receiptFormatter, new List<ICanBeSold>()) { }
 
-	    public decimal Total
-	    {
+		public decimal Total
+		{
 			get { 
 				return _items.Aggregate((decimal) 0, 
 					(subtotal, item) => 
 						subtotal + item.Price + _taxCalculator.CalculateOn(item));
 			}
-	    }
+		}
 
-	    public decimal TotalTaxes
-	    {
+		public decimal TotalTaxes
+		{
 			get
 			{
 				return _items.Aggregate((decimal)0,
 					(subtotal, item) =>
 						subtotal + _taxCalculator.CalculateOn(item));
 			}
-	    }
+		}
 
-	    public string PrintReceipt()
-	    {
-		    foreach (var item in _items)
-		    {
-			    _receiptFormatter.Add(item.Name, item.Price, _taxCalculator.CalculateOn(item));
-		    }
-		    return _receiptFormatter.Print();
-	    }
-    }
+		public string PrintReceipt()
+		{
+			foreach (var item in _items)
+			{
+				_receiptFormatter.Add(item.Name, item.Price, _taxCalculator.CalculateOn(item));
+			}
+			return _receiptFormatter.Print();
+		}
+	}
 }
